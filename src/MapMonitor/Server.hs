79a97311cec45fdfee64bb33e2e1fca98fc541b8
teggotic <teggot.teggot@gmail.com>
2026-05-02 01:42:04 +0300

    add admins

--- src/MapMonitor/Server.hs
+++ src/MapMonitor/Server.hs
@@ -258,7 +258,7 @@ managementApiServer (Authenticated auser) = managementReportMap :<|> managementD
 managementApiServer _ = throwAll err404
 
 trustedUsers :: [Text]
-trustedUsers = ["c331bdbf-2182-4a51-813d-87d6f0f209c5", "65ce1935-d166-42b3-89a6-6345ccf41865", "59b84907-59fb-4455-b31d-b0cc44c36ec7", "bce4d579-dc66-43b5-9d57-eb1fb58dd450", "296a77c2-1c19-4236-9a3e-28c8c01e6312", "52f40bee-ef2e-44b9-baf0-067f39dbc45a"]
+trustedUsers = ["c331bdbf-2182-4a51-813d-87d6f0f209c5", "65ce1935-d166-42b3-89a6-6345ccf41865", "59b84907-59fb-4455-b31d-b0cc44c36ec7", "bce4d579-dc66-43b5-9d57-eb1fb58dd450", "296a77c2-1c19-4236-9a3e-28c8c01e6312", "52f40bee-ef2e-44b9-baf0-067f39dbc45a", "247d5f09-eaa4-4495-9363-b7e69dd42db5", "98b7dfd7-6706-47f4-9b47-6362e9daf7a2"]
 
 authApiServer :: ServerT AuthAPI AppM
 authApiServer = authOpenplanetToken :<|> authIsTrusted
